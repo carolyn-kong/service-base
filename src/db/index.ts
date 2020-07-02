c1cf5e06b995f463cf8fcb2cd95be1a24218fee4
DevelopmentByDavid <developmentbydavid@gmail.com>
2020-07-02 15:17:54 -0700

    minor changes

---
+++ src/db/index.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+export async function connect(): Promise<void> {}
+
+// TODO: replace this with your driver code
+export async function query() {}
